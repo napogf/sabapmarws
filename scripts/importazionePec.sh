e92a23c41bb6bb4a372e5d404455a688189a9c91
Giacomo Fonderico <giacomo.fonderico@gmail.com>
2019-01-28 11:51:29 +0100

    script importazione pec

--- scripts/importazionePec.sh
+++ scripts/importazionePec.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
+rootdir="/var/www"
 cd /var/www/scripts
 phpbin=$(which php)
 scriptpath=$(pwd)
+
 sudo mount -a
 if [ ! -e "/tmp/batchPecLoad.pid" ]; then
-#    if [ ! -e "/home/prasys/www/pecmail/NON_CANCELLARE.TXT" ]; then
-#        echo "[`date`] : importazionePec.sh : il nas non è montato!" >> /home/prasys/www/logs/importazionePec.log
+#    if [ ! -e "$rootdir/pecmail/NON_CANCELLARE.TXT" ]; then
+#        echo "[`date`] : importazionePec.sh : il nas non è montato!" >> $rootdir/logs/importazionePec.log
 #    else
 
         touch /tmp/batchPecLoad.pid
-        $phpbin $scriptpath/batchPecMail.php >> /home/prasys/www/logs/importazionePec.log
-        # $phpbin $scriptpath/batchPecMail.php MAIL >> /home/prasys/www/logs/importazionePec.log
+        $phpbin $scriptpath/batchPecMail.php >> $rootdir/logs/importazionePec.log
+        # $phpbin $scriptpath/batchPecMail.php MAIL >> $rootdir/logs/importazionePec.log
 
-        $phpbin $scriptpath/RicevuteConsegnaAccettazione.php >> /home/prasys/www/logs/RicevuteConsegnaAccettazione.log
+        $phpbin $scriptpath/RicevuteConsegnaAccettazione.php >> $rootdir/logs/RicevuteConsegnaAccettazione.log
 
         rm /tmp/batchPecLoad.pid
 #    fi
 else
-    echo "[`date`] : batchPecLoad.sh : Process is already running" >> /home/prasys/www/logs/importazionePec.log
+    echo "[`date`] : batchPecLoad.sh : Process is already running" >> $rootdir/logs/importazionePec.log
 fi
